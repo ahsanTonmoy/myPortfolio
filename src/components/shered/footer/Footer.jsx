b20c6531004dd358882aa065601e90b1aedfdbe3
Ahsan Tonmoy <ahosant82@gmail.com>
2024-08-14 22:21:56 +0600

    fotter menu position change

--- src/components/shered/footer/Footer.jsx
+++ src/components/shered/footer/Footer.jsx
@@ -6,7 +6,33 @@ import { FaTwitter } from "react-icons/fa";
 import { FaFacebookF } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa";
 import AdbIcon from '@mui/icons-material/Adb';import Menu from "../menu/Menu";
+
+import { usePathname } from 'next/navigation';
  const Footer = () => {
+    const pathName = usePathname();
+    const page = [
+
+        {
+            route: 'home',
+            pathName: '/'
+        },
+        {
+            route: 'about',
+            pathName: '/about'
+        },
+        {
+            route: 'service',
+            pathName: '/service'
+        },
+        {
+            route: 'contact',
+            pathName: '/contact'
+        },
+        {
+            route: 'blogs',
+            pathName: '/blogs'
+        },
+    ]
     return (
         <div>
             <div className=" bg-[#0f0715] m-0">
@@ -31,7 +57,11 @@ import AdbIcon from '@mui/icons-material/Adb';import Menu from "../menu/Menu";
 
                     {/*  */}
                     <Box>
-                        <Menu display={`flex flex-wrap gap-3`}></Menu>
+                    <ul className={` flex flex-wrap text-center gap-4 justify-center font-semibold uppercase text-sm`}>
+                        {page.map((page) => (
+                            <Link key={page} className={`${pathName === page.pathName ? 'hover:bg-gradient-to-l bg-gradient-to-r from-[#6c3ccb] to-[#2a1454] rounded-full px-6 py-2' : 'hover:bg-gradient-to-l from-[#6c3ccb] to-[#2a1454] rounded-full px-6 py-2 lg:hover:ml-2'}`} href={page.pathName}>{page.route}</Link>
+                        ))}
+                    </ul>
                     </Box>
 
                     {/*  */}
