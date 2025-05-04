c24fe2dfff384765c2e4cfffd6a2cc592f5bf53a
Ahsan Tonmoy <ahosant82@gmail.com>
2025-05-04 14:16:04 +0600

    add details page ui

--- src/app/[id]/page.jsx
+++ src/app/[id]/page.jsx
@@ -4,6 +4,9 @@ import React from "react";
 import { projects } from "@/utills/projects";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
+import { FaCaretRight } from "react-icons/fa";
+import Link from "next/link";
+//
 const page = ({ params }) => {
   const pathName = usePathname();
   const id = params.id;
@@ -12,102 +15,127 @@ const page = ({ params }) => {
     <div>
       <div className="bgDark px-4 md:px-20">
         {/* indecetor */}
-        <div className="">
-          <div className="">
-            
+        <div className="py-6">
+          <div className=" flex gap-2 text-xs capitalize text-gray-300">
+            <Link href={"/"} className="border-b">
+              home
+            </Link>
+            <div className="mt-[2px]">
+              <FaCaretRight />
+            </div>
+            {/*  */}
+            <div className="">{singleItem.title}</div>
           </div>
         </div>
         {/* heading */}
-        <div className="grid md:grid-cols-12 gap-8 mb-12 bg-slate-400">
+        <div className="grid md:grid-cols-12 gap-8 ">
           <div className="md:col-span-8">
-            <h1 className="text-3xl md:text-4xl font-bold mb-4 text-gradient">
+            <h1 className="text-3xl md:text-4xl font-bold mb-4 text-gray-100">
               {singleItem.title}
             </h1>
 
-            <div className="flex flex-wrap gap-2 mb-6">
+            <div className="flex flex-wrap gap-2 my-6">
               {singleItem.technologies.map((tech) => (
                 <div
                   key={tech}
-                  variant="outline"
-                  className="bg-dark-surface border-highlight text-highlight"
+                  className="uppercase text-xs font-bold h-6 py-1 px-4 bg-black w-fit rounded-full border border-[#6c3ccb] text-[#6c3ccb] cursor-pointer"
                 >
                   {tech}
                 </div>
               ))}
             </div>
 
-            <p className="text-gray-300 mb-8 leading-relaxed">
+            {/* <p className="text-gray-300 mb-8 leading-relaxed">
               {singleItem.description}
-            </p>
+            </p> */}
 
             <div className="flex flex-wrap gap-4">
               <button
-                className="bg-gradient hover:opacity-90 transition-opacity flex items-center gap-2"
-                // onClick={() => window.open(singleItem.demo, '_blank')}
+                className="bg-[#6c3ccb] hover:bg-gradient-to-l from-[#6c3ccb] to-[#2a1454] border-0 hover:border-0 border-[#6c3ccb] text-white font-bold uppercase rounded-md px-8 py-2 "
+                onClick={() => window.open(singleItem.demo, "_blank")}
               >
                 {/* <ExternalLinkIcon size={16} /> */}
                 Live Demo
               </button>
               <button
-                variant="outline"
-                className="border-highlight text-highlight hover:bg-highlight/10"
-                // onClick={() => window.open(singleItem.github, '_blank')}
+                className="bg-[#6c3ccb] hover:bg-gradient-to-l from-[#6c3ccb] to-[#2a1454] border-0 hover:border-0 border-[#6c3ccb] text-white font-bold uppercase rounded-md px-8 py-2 "
+                variant="outlined"
+                onClick={() => window.open(singleItem.github, "_blank")}
               >
                 {/* <GithubIcon size={16} className="mr-2" /> */}
                 View Code
               </button>
             </div>
           </div>
-
-          <div className="md:col-span-4">
-            <div className="bg-dark-surface border-gray-800 overflow-hidden">
-              <div className="p-6">
-                <h3 className="text-lg font-semibold mb-4 text-gradient">
-                  singleItem Details
-                </h3>
-                <div className="space-y-4">
-                  <div>
-                    <p className="text-gray-400 text-sm">Category</p>
-                    <p className="text-white">{singleItem.category}</p>
-                  </div>
-                  <div>
-                    <p className="text-gray-400 text-sm">Completed</p>
-                    <p className="text-white">{singleItem.completedDate}</p>
-                  </div>
-                  <div>
-                    <p className="text-gray-400 text-sm">Role</p>
-                    <p className="text-white">{singleItem.role}</p>
+        </div>
+        {/* content */}
+        <div className="relative py-12">
+          <div className="grid md:grid-cols-2 gap-4">
+            {/* left side  */}
+            <div className=" rounded-lg border border-gray-800 h-96">
+              <Image
+                src={singleItem.image}
+                alt={singleItem.title}
+                className="w-full h-full"
+              />
+            </div>
+            {/* right side */}
+            <div className=" text-white p-6">
+              {/*  */}
+              
+              {/*  */}
+              <div className="prose prose-invert ">
+                {/* <div className=" capitalize  text-gray-400 text-base ">
+                Key Features
+              </div> */}
+                <ul className="space-y-3 py-4">
+                  {singleItem.features.map((feature, index) => (
+                    <li
+                      key={index}
+                      className="flex items-start gap-2 text-sm text-gray-200  font-bold"
+                    >
+                      <span className="text-bold ">✓</span>
+                      <span>{feature}</span>
+                    </li>
+                  ))}
+                </ul>
+                <div className="">
+                <div className="bg-dark-surface border-gray-800 overflow-hidden">
+                  <div className="p-6">
+                    <div className="space-y-1">
+                      <div>
+                        <p className="text-gray-400 text-sm">Category</p>
+                        <p className="text-white">{singleItem.category}</p>
+                      </div>
+                      <div>
+                        <p className="text-gray-400 text-sm">Completed</p>
+                        <p className="text-white">{singleItem.completedDate}</p>
+                      </div>
+                      <div>
+                        <p className="text-gray-400 text-sm">Role</p>
+                        <p className="text-white">{singleItem.role}</p>
+                      </div>
+                    </div>
                   </div>
                 </div>
+              </div>
               </div>
             </div>
           </div>
         </div>
-        {/* content */}
-        <div className="relative mb-12">
-          <div className="aspect-[16/9] overflow-hidden rounded-lg border border-gray-800">
-            <Image
-              src={singleItem.image}
-              alt={singleItem.title}
-              fill
-              className="w-full h-full object-cover"
-            />
-          </div>
-        </div>
+        <div className="bg-dark-surface border border-gray-800 my-12"></div>
         {/* footer */}
-        <div defaultValue="overview" className="mb-12">
-          <div className="bg-dark-surface border border-gray-800 mb-6"></div>
-
-          <div
-            value="overview"
-            className="border border-gray-800 rounded-md p-6 bg-dark-surface"
-          >
+        <div defaultValue="overview" className=" my-10">
+          {/*  */}
+          <div value="overview" className="  rounded-md p-6">
             <div className="prose prose-invert max-w-none">
-              <h2 className="text-2xl font-bold mb-4">singleItem Overview</h2>
+              <h2 className="capitalize  text-gray-400 text-base py-2">
+                project Overview
+              </h2>
               <p className="mb-4">{singleItem.overview}</p>
               {singleItem.problemStatement && (
                 <>
-                  <h3 className="text-xl font-semibold mb-3">
+                  <h3 className="capitalize  text-gray-400 text-base py-2">
                     Problem Statement
                   </h3>
                   <p>{singleItem.problemStatement}</p>
@@ -115,36 +143,38 @@ const page = ({ params }) => {
               )}
             </div>
           </div>
-
-          <div
-            value="features"
-            className="border border-gray-800 rounded-md p-6 bg-dark-surface"
-          >
-            <div className="prose prose-invert max-w-none">
-              <h2 className="text-2xl font-bold mb-4">Key Features</h2>
-              <ul className="space-y-2">
+          {/*  */}
+          <div value="features" className=" rounded-md px-6 bg-dark-surface">
+            <div className="prose prose-invert ">
+              <div className=" capitalize  text-gray-400 text-base ">
+                Key Features
+              </div>
+              <ul className="space-y-3 py-4">
                 {singleItem.features.map((feature, index) => (
-                  <li key={index} className="flex items-start gap-2">
-                    <span className="text-highlight mt-1">▹</span>
+                  <li
+                    key={index}
+                    className="flex items-start gap-2 text-xs text-gray-200  font-bold"
+                  >
+                    <span className="text-highlight mt-[2px]">▹</span>
                     <span>{feature}</span>
                   </li>
                 ))}
               </ul>
             </div>
           </div>
+          {/*  */}
 
-          <div className="border border-gray-800 rounded-md p-6 bg-dark-surface">
+          <div className="rounded-md p-6 bg-dark-surface">
             <div className="prose prose-invert max-w-none">
-              <h2 className="text-2xl font-bold mb-4">
-                Challenges & Solutions
-              </h2>
               {singleItem.challenges.map((challenge, index) => (
                 <div key={index} className="mb-6">
-                  <h3 className="text-xl font-semibold mb-2">
+                  <h3 className="capitalize  text-gray-400 text-base py-2">
                     {challenge.title}
                   </h3>
                   <p className="mb-2">{challenge.description}</p>
-                  <p className="text-highlight font-semibold">Solution:</p>
+                  <p className="capitalize  text-gray-400 text-base py-2">
+                    Solution:
+                  </p>
                   <p>{challenge.solution}</p>
                 </div>
               ))}
